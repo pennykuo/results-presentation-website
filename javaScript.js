3148b1505f7f799f954582ae9a5fb3bbca4f3949
GETACAD\penny.kuo <penny.kuo@getac.cm.tw>
2024-03-28 17:17:45 +0800

    1.更改作品的版面 2.把成果發表大綱添加(版面會再調整

--- javaScript.js
+++ javaScript.js
@@ -1,96 +1,28 @@
-$(window).scroll(function (e) {
-    if ($(window).scrollTop() <= 0) {
-        $(".explore, .navbar").addClass("at_top");
-    } else {
-        $(".explore, .navbar").removeClass("at_top");
-    }
+
+$(document).ready(function() {
+    $(".process-text").mouseover(function() {
+        // 當滑鼠移動到元素上時執行的代碼
+        $(".process-content").css("left", 0);
+        console.log("滑鼠已經移動到'成果發表大綱'上");
+    });
+
+    $(".process-text").mouseout(function() {
+        // 當滑鼠移出元素時執行的代碼
+        $(".process-content").css("left", "-1600px");
+        console.log("滑鼠已經離開'成果發表大綱'");
+    });
 });
 
-$(document).on('click', 'a', function (event) {
-    event.preventDefault();
-    var target = $(this).attr("href");
+$(document).ready(function() {
+    $(".publish-text").mouseover(function() {
+        // 當滑鼠移動到元素上時執行的代碼
+        $(".publish-content").css("left", 0);
+        console.log("滑鼠已經移動到'成果發表大綱'上");
+    });
 
-    $('html,body').animate({
-        scrollTop: $(target).offset().top
-    }, 500);
+    $(".publish-text").mouseout(function() {
+        // 當滑鼠移出元素時執行的代碼
+        $(".publish-content").css("left", "-1600px");
+        console.log("滑鼠已經離開'成果發表大綱'");
+    });
 });
-
-function detect(name, x) {
-    var windowWidth = $(window).width();
-    var elementWidth = $(name).width();
-
-    var center = (windowWidth - elementWidth) / 2;
-    if (x > 0 && x < 400) { 
-        $(name).css("left",0); 
-    } else {
-        $(name).css("left", "-1600px"); 
-    }
-}
-
-function right_detect(name, x) {
-    var windowWidth = $(window).width();
-    var elementWidth = $(name).width();
-
-    var center = (windowWidth - elementWidth) / 2;
-    if (x > 800 && x < 1200) {
-        $(name).css("right",0); 
-    } else {
-        $(name).css("right", "-1600px"); 
-    }
-}
-//滑鼠移動事件
-$(window).mousemove(function (evt) {
-    var pagex = evt.pageX;
-    var pagey = evt.pageY;
-
-    $(".boat").css("transform", "translateX(" + (pagex / -20 + 30) + "px) scale(1.2)");
-
-    detect(".process-content", pagex);
-    detect(".roro", pagex);
-    right_detect(".publish-content", pagex);
-    right_detect(".roro2", pagex);
-    
-});
-
-var worksdata = [
-    { url: "./image/work1.jpg", text: "Penny-Recipes" },
-    { url: "./image/work2.jpg", text: "The Penny Animals" },
-    { url: "./image/work3.jpg", text: "Rock Paper Scissors Game" },
-    { url: "./image/work4.jpg", text: "Personal Information" },
-];
-
-var vm = new Vue({
-    el: '#works',
-    data: {
-        works: worksdata
-    }
-});
-
-/* teams area */
-var $cont = document.querySelector('.cont');
-var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
-
-setTimeout(function() {
-  $cont.classList.remove('s--inactive');
-}, 200);
-
-$elsArr.forEach(function($el) {
-  $el.addEventListener('click', function() {
-    if (this.classList.contains('s--active')) return;
-    $cont.classList.add('s--el-active');
-    this.classList.add('s--active');
-  });
-});
-
-$closeBtnsArr.forEach(function($btn) {
-  $btn.addEventListener('click', function(e) {
-    e.stopPropagation();
-    $cont.classList.remove('s--el-active');
-    document.querySelector('.el.s--active').classList.remove('s--active');
-  });
-});
-function flipPage(){
-    document.body.classList.toggle('flipped');
-    window.scrollTo(0, 0);
-}
---
+++ teamsScript.js
@@ -0,0 +1,28 @@
+/* teams area */
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+  });
+});
+
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
+  });
+});
+function flipPage(){
+    document.body.classList.toggle('flipped');
+    window.scrollTo(0, 0);
+}
